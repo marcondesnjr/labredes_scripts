31a227889e2ed7b45464b394a200de04d6124dc3
Marcondes Junior <junior3917@gmail.com>
2025-02-05 20:43:47 -0300

    DMU Descriptions Dataframe

--- script.r
+++ script.r
@@ -1,13 +1,20 @@
 library(pracma)
 library(fractaldim)
 library(ptsuite)
-
-BASE_DIR <- file.path("/","home","jmnj","Área de Trabalho","sc")
+BASE_DIR <- file.path("/","home","jmnj","labredes","sctests")
 DATA_DIR <- file.path(BASE_DIR, "data")
 
-DMUDataFrames <- c()
 
 scaleVector <- c()
+dmuindex <- 1
+
+DMUDesc = data.frame(
+  DMU = c(),
+  vram = c(),
+  server = c(),
+  alg = c()
+  
+)
 
 DMUDataFrame = data.frame(
   DMU = c(),
@@ -24,6 +31,9 @@ for(ram in rams){
   for(server in servers){
     algs <- list.dirs(path = server, full.names = TRUE, recursive = FALSE)
     for(alg in algs){
+      DMU <- c(paste("DMU",dmuindex, sep = ""), basename(ram), basename(server), basename(alg))
+      DMUDesc <- rbind(DMUDesc, DMU)
+      dmuindex <- dmuindex + 1
       scales <- list.dirs(path = alg, full.names = TRUE, recursive = FALSE)
         for(scale in scales){
           scaleName <- basename(scale)
@@ -33,7 +43,7 @@ for(ram in rams){
           }
           
           #######################
-          DMU <- paste(basename(ram), basename(server), basename(alg), basename(scale), sep = "_")
+          DMUId <- paste(DMU[1] ,basename(scale), sep = "_")
           statsDir <- file.path(scale, "stats")
           
           timeToTest <- scan(file = file.path(scale, "stats", "time_taken_for_tests.txt"))
@@ -44,7 +54,7 @@ for(ram in rams){
           hurst <- hurstexp(timeSeries)[1]
           alfaTailShape <- alpha_mle(timeSeries)[1]
           
-          row <- data.frame(DMU, fdim, timeToTest, transferRate, requestPerSecond, hurst, alfaTailShape)
+          row <- data.frame(DMUId, fdim, timeToTest, transferRate, requestPerSecond, hurst, alfaTailShape)
           
           DMUDataFrame <- rbind(DMUDataFrame, row)
       }
@@ -53,8 +63,10 @@ for(ram in rams){
 }
 
 colnames(DMUDataFrame) <- c("DMU", "Fractal Dimension" ,"Time Taken To Tests", "Transfer Rate", "Requests Per Second", "Hurst Parameter", "Alfa Tail Shape" )
+colnames(DMUDesc) <- c("Name", "VRAM" ,"Server", "Algorithm")
 
 for(scaleName in scaleVector){
   filtered <- subset(DMUDataFrame, grepl(scaleName, DMU))
-  write.table(filtered, file.path(BASE_DIR, paste(scaleName, ".csv")), sep = ",", row.names = FALSE, col.names = TRUE)
+  filtered$DMU <- gsub(paste("_", scaleName, sep = ""),"",as.character(filtered$DMU))
+  write.table(filtered, file.path(BASE_DIR, paste(scaleName, ".csv", sep = "")), sep = ",", row.names = FALSE, col.names = TRUE)
 }
